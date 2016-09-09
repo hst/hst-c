9dc3465627fb1e29f123acac5360f3be94188b70
Douglas Creager <dcreager@dcreager.net>
2016-09-09 10:12:22 -0400

    Add initial autotools template
    They're pervasive, so let's just use the autotools.  These build scripts
    are based on the Autotools Mythbuster [1], and build a simple no-op
    `hst` program.
    
    [1] https://autotools.io/index.html

---
+++ src/hst/hst.c
@@ -0,0 +1,12 @@
+/* -*- coding: utf-8 -*-
+ * -----------------------------------------------------------------------------
+ * Copyright © 2016, HST Project.
+ * Please see the COPYING file in this distribution for license details.
+ * -----------------------------------------------------------------------------
+ */
+
+int
+main(int argc, char** argv)
+{
+    return 0;
+}
