a5f0e4ff804c70b0d1ff2ca8b16cdaf479a754f5
Douglas Creager <dcreager@dcreager.net>
2016-11-02 16:09:01 -0400

    Test in-source and out-of-source builds

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -ev
-mkdir .build
-cd .build
-../autogen.sh
-../configure --enable-valgrind CFLAGS="-g -O3 -flto" LDFLAGS="-flto"
+
+srcdir="$PWD"
+
+if [ ${BUILD_TYPE} = out_of_source ]; then
+    mkdir .build
+    cd .build
+fi
+
+${srcdir}/autogen.sh
+${srcdir}/configure --enable-valgrind CFLAGS="-g -O3 -flto" LDFLAGS="-flto"
 make
 make check
 make check-valgrind
