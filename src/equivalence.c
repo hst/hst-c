59be561997e0afe8e2adbd95d08842ac29d8ec3c
Douglas Creager <dcreager@dcreager.net>
2016-12-28 16:12:24 -0500

    Update equivalences to use new data structures

--- src/equivalence.c
+++ src/equivalence.c
@@ -5,56 +5,39 @@
  * -----------------------------------------------------------------------------
  */
 
+#include "equivalence.h"
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define JUDYERROR_NOTEST 1
-#include <Judy.h>
-
-#include "ccan/build_assert/build_assert.h"
-#include "ccan/compiler/compiler.h"
-#include "ccan/likely/likely.h"
-#include "hst.h"
+#include "id-map.h"
+#include "id-set-map.h"
+#include "id-set.h"
 
 void
 csp_equivalences_init(struct csp_equivalences *equiv)
 {
-    equiv->classes = NULL;
-    equiv->members = NULL;
+    csp_id_set_map_init(&equiv->classes);
+    csp_id_map_init(&equiv->members);
 }
 
 void
 csp_equivalences_done(struct csp_equivalences *equiv)
 {
-    UNNEEDED Word_t dummy;
-    Word_t *vmembers;
-    csp_id class_id = 0;
-    JLF(vmembers, equiv->classes, class_id);
-    while (vmembers != NULL) {
-        void *members = (void *) *vmembers;
-        J1FA(dummy, members);
-        JLN(vmembers, equiv->classes, class_id);
-    }
-    JLFA(dummy, equiv->classes);
-    JLFA(dummy, equiv->members);
+    csp_id_set_map_done(&equiv->classes);
+    csp_id_map_done(&equiv->members);
 }
 
 void
 csp_equivalences_add(struct csp_equivalences *equiv, csp_id class_id,
                      csp_id member_id)
 {
-    Word_t *vclass;
-    Word_t old_class_id;
-    Word_t *vmembers;
-    void **members;
-    UNNEEDED int rc;
+    csp_id old_class_id;
 
     /* First insert the member_id into the `members` map.  As a side effect,
      * this tells us if the member was already in an equivalence class. */
-    JLI(vclass, equiv->members, member_id);
-    old_class_id = *vclass;
-    *vclass = class_id;
+    old_class_id = csp_id_map_insert(&equiv->members, member_id, class_id);
 
     /* If the member was already in this same equivalence class, there's nothing
      * to do. */
@@ -64,57 +47,36 @@ csp_equivalences_add(struct csp_equivalences *equiv, csp_id class_id,
 
     /* If the member was already in a DIFFERENT equivalence class, remove it. */
     if (old_class_id != 0) {
-        JLG(vmembers, equiv->classes, old_class_id);
-        assert(vmembers != NULL);
-        members = (void **) vmembers;
-        J1U(rc, *members, member_id);
+        csp_id_set_map_remove(&equiv->classes, old_class_id, member_id);
     }
 
     /* And then add the member to the new equivalence class. */
-    JLI(vmembers, equiv->classes, class_id);
-    members = (void **) vmembers;
-    J1S(rc, *members, member_id);
+    csp_id_set_map_insert(&equiv->classes, class_id, member_id);
 }
 
 void
 csp_equivalences_build_classes(struct csp_equivalences *equiv,
                                struct csp_id_set *set)
 {
-    Word_t *vmembers;
-    csp_id class_id = 0;
-    JLF(vmembers, equiv->classes, class_id);
-    while (vmembers != NULL) {
-        csp_id_set_add(set, class_id);
-        JLN(vmembers, equiv->classes, class_id);
+    struct csp_id_set_map_iterator iter;
+    csp_id_set_map_foreach (&equiv->classes, &iter) {
+        csp_id_set_add(set, csp_id_set_map_iterator_get_from(&iter));
     }
 }
 
 csp_id
 csp_equivalences_get_class(struct csp_equivalences *equiv, csp_id member_id)
 {
-    Word_t *vclass;
-    JLG(vclass, equiv->members, member_id);
-    if (vclass == NULL) {
-        return CSP_ID_NONE;
-    } else {
-        return *vclass;
-    }
+    return csp_id_map_get(&equiv->members, member_id);
 }
 
 void
 csp_equivalences_build_members(struct csp_equivalences *equiv, csp_id class_id,
                                struct csp_id_set *set)
 {
-    Word_t *vmembers;
-    JLG(vmembers, equiv->classes, class_id);
-    if (vmembers != NULL) {
-        void **members = (void **) vmembers;
-        int found;
-        csp_id member_id = 0;
-        J1F(found, *members, member_id);
-        while (found) {
-            csp_id_set_add(set, member_id);
-            J1N(found, *members, member_id);
-        }
+    const struct csp_id_set *members =
+            csp_id_set_map_get(&equiv->classes, class_id);
+    if (members != NULL) {
+        csp_id_set_union(set, members);
     }
 }
--- src/hst.h
+++ src/hst.h
@@ -16,6 +16,7 @@ extern "C" {
 #include <stddef.h>
 #include <stdint.h>
 
+#include "equivalence.h"
 #include "id-set.h"
 
 /*------------------------------------------------------------------------------
@@ -168,46 +169,6 @@ csp_id_pair_set_builder_add(struct csp_id_pair_set_builder *builder,
 void
 csp_id_pair_set_build(struct csp_id_pair_set *set,
                       struct csp_id_pair_set_builder *builder);
-
-/*------------------------------------------------------------------------------
- * Equivalences
- */
-
-/* Stores information about the "equivalence classes" of a set of processes.
- * All of the processes that have "equivalent" behavior (according to one of
- * CSP's semantic models) belong to the same equivalence class. */
-struct csp_equivalences {
-    void *classes;
-    void *members;
-};
-
-void
-csp_equivalences_init(struct csp_equivalences *equiv);
-
-void
-csp_equivalences_done(struct csp_equivalences *equiv);
-
-/* Add a member to an equivalence class.  If the member was already in an
- * equivalence class, it is removed from that one before adding it to the new
- * one. */
-void
-csp_equivalences_add(struct csp_equivalences *equiv, csp_id class_id,
-                     csp_id member_id);
-
-/* Add the IDs of all of the equivalence classes to a set. */
-void
-csp_equivalences_build_classes(struct csp_equivalences *equiv,
-                               struct csp_id_set *set);
-
-/* Return the class that a member belongs to, or CSP_ID_NONE if that member
- * hasn't been added to an equivalence class yet. */
-csp_id
-csp_equivalences_get_class(struct csp_equivalences *equiv, csp_id member_id);
-
-/* Add all of the members of an equivalence class to a set. */
-void
-csp_equivalences_build_members(struct csp_equivalences *equiv, csp_id class_id,
-                               struct csp_id_set *set);
 
 /*------------------------------------------------------------------------------
  * Processes
